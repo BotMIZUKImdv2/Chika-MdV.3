bb3976fd9e3cd0e7f03638d5bc8a0eca15c57550
BotMIZUKImdv2 <102995614+BotMIZUKImdv2@users.noreply.github.com>
2022-04-10 20:40:19 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,18 +18,18 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6281575886399','6281575886399','6281575886399']
-global.premium = ['6281575886399']
-global.ownernomer = '6281575886399'
-global.ownername = 'RiychDwayne'
-global.botname = 'Chika-Md'
+global.owner = ['6282239202895','6282239202895','6282239202895']
+global.premium = ['6282239202895']
+global.ownernomer = '6282239202895'
+global.ownername = 'Alwi Official'
+global.botname = 'Bobl-Md'
 global.footer = 'RiychDwayne'
-global.ig = 'https://instagram.com/riychdwayne'
-global.email = 'riychh.20@gmail.com'
+global.ig = 'https://instagram.com/Alwiganz'
+global.email = 'Alwioffcial@gmail.com'
 global.region = 'Indonesia'
 global.sc = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
 global.myweb = 'https://api-riychdwayne.herokuap.com'
-global.packname = 'Chika-Md'
+global.packname = 'Bobo-Md'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'chika'
 global.prefa = ['','!','.','🐦','🐤','🗿']
